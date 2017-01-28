f5f9760d96f6a18fbc628e3361c7999252213ee6
Attila Bagossy <bagossyattila@outlook.com>
2017-01-28 23:00:23 +0100

    Implemented the first version of the reaction flow

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
+function permute(permutation) {
+  var length = permutation.length,
+      result = new Array([0, 1, 2, 6, 24, 120, 720, 5040, 40320, 362880, 3628800, 39916800, 479001600][length]),
+      c = new Array(length).fill(0),
+      i = 1,
+      j = 1;
+
+  result[0] = permutation.slice();
+  while (i < length) {
+    if (c[i] < i) {
+      var k = (i % 2) ? c[i] : 0,
+          p = permutation[i];
+      permutation[i] = permutation[k];
+      permutation[k] = p;
+      ++c[i];
+      i = 1;
+      result[j] = permutation.slice();
+      ++j;
+    } else {
+      c[i] = 0;
+      ++i;
+    }
+  }
+
+  return result;
+}
+
 export const Reagent =  {
   t() {
     return true;
@@ -38,12 +65,29 @@ export const Solution = {
 
     return obj;
   },
-  extract() {
-    const promises = [];
-    
-    const solutionPromises = this.removeAndGetSolutions().map(s => s.extract());
+  react() {    
+    const solutionPromises = this.removeAndGetSolutions()
+      .map(s => s.react());
 
-    return Promise.all(solutionPromises);
+    const reagentPromises = this.removeAndGetReactions()
+      .map(r => this.executeReaction(r));
+
+    const promises = solutionPromises.concat(reagentPromises)
+      .map(p => p.then(result => this.incorporateResult(result)));
+
+    return Promise.all(promises);
+  },
+  incorporateResult(result) {
+    if (Array.isArray(results)) {
+      this.multiset.push(...results);
+    } else if (result !== undefined) {
+      this.multiset.push(result);
+    }
+  },
+  executeReaction(reaction) {
+    return new Promise(function promise(resolve) {
+      resolve(reaction.reagent.action(...reaction.args));
+    });
   },
   removeAndGetSolutions() {
     let i = this.multiset.length;
@@ -57,5 +101,44 @@ export const Solution = {
     }
 
     return solutions;
+  },
+  removeAndGetReactions() {
+    let i = this.multiset.length;
+
+    const reactions = [];
+
+    while (i--) {
+      if (Object.prototype.isPrototypeOf.call(Reagent, this.multiset[i])) {
+        const reagent = this.multiset.splice(index, 1)[0];
+
+        const args = findMatchingArguments(reagent);
+
+        if (!args) {
+          this.multiset.push(reagent);
+        } else {
+          reactions.push({ reagent, args });
+        }
+      }
+    }
+    
+    return reactions;
+  },
+  findMatchingArguments(reagent) {
+    if (reagent.args == 0) {
+      return [];
+    }
+
+    for (let i = 0; i < this.multiset.length - reagent.args; i++) {
+       const args = permute(this.multiset.slice(i, i + reagent.args + 1))
+          .find(a => reagent.condition(...a));
+
+       if (args) {
+         this.multiset.splice(i, reagent.args);
+
+         return args;
+       }
+    }
+
+    return null;
   }
 };
