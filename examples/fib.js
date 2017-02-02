dc4dc97e0d468e7e3b60adfd9cbb28302b3ee7ce
Attila Bagossy <bagossyattila@outlook.com>
2017-02-02 01:00:01 +0100

    fib.js now uses seq

--- examples/fib.js
+++ examples/fib.js
@@ -2,8 +2,6 @@ const Burette = require('../lib/burette.js');
 const Solution = Burette.Solution;
 const Reagent = Burette.Reagent;
 
-const fibOf = 10;
-
 const dec = Reagent.of({
   condition: x =>  x > 1,
   action: x => [x - 1, x - 2]
@@ -13,8 +11,6 @@ const add = Reagent.of({
   action: (x, y) => x + y
 }).nShot();
 
-const createParts = Solution.of([fibOf, dec]);
-
-Solution.of([createParts, add], { mergeReagents: false })
+Solution.seq([10, dec], add)
   .react()
   .then(s => console.log(s.multiset));
