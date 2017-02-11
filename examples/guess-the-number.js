b14a1c1cb10e4e9bba8f22674590725a01695bf2
Attila Bagossy <bagossyattila@outlook.com>
2017-02-11 15:29:18 +0100

    Refactored 'Guess the number' to make it easier to understand

--- examples/guess-the-number.js
+++ examples/guess-the-number.js
@@ -1,9 +1,11 @@
-const Burette = require('../lib/burette.js');
-const Solution = Burette.Solution;
 const process = require('process');
+
+const Joi = require('joi');
+const Burette = require('../lib/burette.js');
+
+const Solution = Burette.Solution;
 const Reagent = Burette.Reagent;
 const Catalyst = Burette.Catalyst;
-const Joi = require('joi');
 
 const stdinInput = function stdinInput(propagator) {
   const obj = {
@@ -14,35 +16,45 @@ const stdinInput = function stdinInput(propagator) {
 
   process.stdin.on('readable', () => {
     const chunk = process.stdin.read();
+
     if (chunk !== null) {
-      obj.propagator(chunk);
+      obj.propagator({
+        guess: Number.parseInt(chunk)
+      });
     }
   });
 
   return obj;
 };
 
-const shape = [Joi.number(), Joi.string()];
+const target = {
+  target: 50
+};
+
+const shape = [
+  Joi.object().keys({ target: Joi.number() }),
+  Joi.object().keys({ guess: Joi.number() })
+];
 
 const high = Reagent.of({
   shape,
-  condition:  (x, y) => parseInt(y) > x,
-  action: x => [Catalyst.of(() => console.log('Too high!')), x]
+  condition:  (t, g) => t.target < g.guess,
+  action: t => [Catalyst.of(() => console.log('Too high!')), t]
 }).nShot();
 
 const low = Reagent.of({
   shape,
-  condition:  (x, y) => parseInt(y) < x,
-  action: x => [Catalyst.of(() => console.log('Too low!')), x]
+  condition:  (t, g) => t.target > g.guess,
+  action: t => [Catalyst.of(() => console.log('Too low!')), t]
 }).nShot();
 
 const match = Reagent.of({
   shape,
-  condition: (x, y) => parseInt(y) == x,
+  condition:  (t, g) => t.target == g.guess,
   action: () => Catalyst.of(() => { 
     console.log('That\'s it!');
 
-    process.exitCode = 0;
+    process.exit(0);
   })
 });
 
@@ -50,7 +62,7 @@ const validator = function validator(value, schema) {
   return !Joi.validate(value, schema, { convert: false }).error;
 };
 
-Solution.of([high, low, match, 50])
+Solution.of([high, low, match, target])
         .setShapeValidator(validator)
         .applyValidatorToSubsolutions()
         .input(stdinInput)
